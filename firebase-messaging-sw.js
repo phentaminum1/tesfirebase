ca69383690fc127dc77d52f714d4e0d6738e182b
phentaminum1 <rama.ar4u@gmail.com>
2025-12-31 09:34:09 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,20 +5,8 @@ firebase.initializeApp({
   apiKey: "AIzaSyC5bH3EHcHFrjstgQ2ZLCf_Q2JdWMdOjF4",
   authDomain: "inven-bafira.firebaseapp.com",
   projectId: "inven-bafira",
-  storageBucket: "inven-bafira.firebasestorage.app",
   messagingSenderId: "1081146624082",
   appId: "1:1081146624082:web:fd893dcb20c6624004778b"
 });
 
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(payload => {
-  console.log("📩 Background message:", payload);
-
-  self.registration.showNotification(
-    payload.notification?.title || "Notifikasi",
-    {
-      body: payload.notification?.body || "",
-    }
-  );
-});
+firebase.messaging();
